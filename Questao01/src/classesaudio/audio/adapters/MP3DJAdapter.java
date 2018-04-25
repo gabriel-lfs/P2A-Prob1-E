13c9f8efc9091942f638575d53cf15cc49056246
Gabriel Luís Fernando de Souza <gabriel-soz@live.com>
2018-04-24 21:09:58 -0300

    Criação do Adapter para MP3.

--- Questao01/src/classesaudio/audio/adapters/MP3DJAdapter.java
+++ Questao01/src/classesaudio/audio/adapters/MP3DJAdapter.java
@@ -13,34 +13,43 @@ import problema1.MP3DJ;
  */
 public class MP3DJAdapter extends FormatoAudioAdapter {
 
-    private MP3DJ player;
-
+    private final MP3DJ player = new MP3DJ();
     @Override
     public void abrir(String nomeArquivo) {
+        player.setFile(nomeArquivo);
     }
 
     @Override
     public void reproduzir() {
+        player.play();
     }
 
     @Override
     public void pausar() {
+         player.stop();
+        
     }
 
     @Override
     public void parar() {
+        player.stop();
+        int qtdeRetornar = player.reward(0);
+        player.reward(qtdeRetornar);
     }
 
     @Override
     public void avancar(int ponteiro) {
+        player.forward(ponteiro);
     }
 
     @Override
     public void retornar(int ponteiro) {
+        player.reward(ponteiro);
     }
 
     @Override
     public void liberar() {
+        player.stop();
+        player.setFile(null);
     }
-
 }
